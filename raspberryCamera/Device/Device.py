06fd86853c034e3875c54a8b8f3a0b299f1ac398
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2017-05-09 17:41:12 -0500

    minor fixes

--- raspberryCamera/Device/Device.py
+++ raspberryCamera/Device/Device.py
@@ -42,7 +42,7 @@ class Device:
         self.broker.subscribeTopicWithCallback("ping", self.registration )
 
         #adding camera
-        self.sensors.append(Camera.Camera(storage=self.storage, broker=self.broker, id, enabled))
+        self.sensors.append(Camera.Camera(storage=self.storage, broker=self.broker, id=0, enabled=True))
 
     def registration(self):
         self.console.log("Sending registration...")
